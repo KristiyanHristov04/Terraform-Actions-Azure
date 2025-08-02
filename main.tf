3d97469e2dfe07be786ee06f672740fb5f9f6c13
Kristiyan <kristiyan_hristov04@abv.bg>
2025-08-02 11:53:17 +0300

    Remove storage file

--- main.tf
+++ main.tf
@@ -95,3 +95,22 @@ resource "azurerm_app_service_source_control" "taskboardsc" {
   branch                 = "master"
   use_manual_integration = true
 }
+
+resource "azurerm_resource_group" "mystoragerg" {
+  name     = "MyStorageRG2"
+  location = "North Europe"
+}
+
+resource "azurerm_storage_account" "mystorageaccount" {
+  name                     = "taskboardsa2025"
+  resource_group_name      = azurerm_resource_group.mystoragerg.name
+  location                 = azurerm_resource_group.mystoragerg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "mystoragecontainer" {
+  name                  = "taskboardsc2025"
+  storage_account_id    = azurerm_storage_account.mystorageaccount.id
+  container_access_type = "private"
+}
