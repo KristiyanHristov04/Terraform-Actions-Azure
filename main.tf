a35d9a6225b32d540120a4f987dca940b3703f21
Kristiyan <kristiyan_hristov04@abv.bg>
2025-08-02 10:51:11 +0300

    Modified names once again and .yml files

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "azurerm_resource_group" "mystoragerg" {
 }
 
 resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "mytaskboardsa-${random_integer.ri.result}"
+  name                     = "taskboardsa${random_integer.ri.result}"
   resource_group_name      = azurerm_resource_group.mystoragerg.name
   location                 = azurerm_resource_group.mystoragerg.location
   account_tier             = "Standard"
@@ -110,7 +110,7 @@ resource "azurerm_storage_account" "mystorageaccount" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = "mytaskboardsc-${random_integer.ri.result}"
+  name                  = "taskboardsc${random_integer.ri.result}"
   storage_account_id    = azurerm_storage_account.mystorageaccount.id
   container_access_type = "private"
 }
